42b42ee8518ac16039646297c4bfb844ea5eb277
Aaron Jiang <nxcleo@gmail.com>
2021-04-10 12:25:02 +1000

    Update neummy_jimtron.py

--- neummy_jimtron.py
+++ neummy_jimtron.py
@@ -13,23 +13,57 @@ def play_turn(game_map: Map, me: Player, opponent: Player, items: list, new_item
     """
     head example:
         (row, col)
-    :param game_map:
-    :param me:
-    :param opponent:
-    :param items:
-    :param new_items:
-    :param heatmap:
-    :param remaining_turns:
-    :return:
     """
     start = timer()
+    max_move, max_value = path_search(game_map, me, opponent, items, new_items, heatmap, remaining_turns,
+                                      depth=50, vehicle=None)
+    print(timer() - start)
+    return f'{max_move[0]},{max_move[1]}'
+
+
+
+
+def play_auction(game_map: Map, me: Player, opponent: Player, items: list, new_items: list, heatmap, remaining_turns):
+    return 0
+
+
+def play_transport(game_map: Map, me: Player, opponent: Player, items: list, new_items: list, heatmap, remaining_turns):
+    return f'{random.randint(0, game_map.rows-1)},{random.randint(0, game_map.cols-1)}'
+
+
+def generate_moves(game_map, origin, vehicle=None):
+    if vehicle is None:
+        moves = []
+        for dir in [1, -1]:
+            for axis in [0, 1]:
+                move = [0, 0]
+                move[axis] = dir
+                new_pos = tuple([origin[i] + move[i] for i in range(2)])
+                if new_pos[1] in range(game_map.cols) and \
+                        new_pos[0] in range(game_map.rows) and \
+                        game_map.is_free(*new_pos):
+                    moves.append(new_pos)
+    elif vehicle == "bike":
+        moves = {origin}
+        for i in range(3):
+            new_moves = set()
+            for move in moves:
+                for position in generate_moves(game_map, move):
+                    new_moves.add(position)
+            moves = new_moves
+    return moves
+
+
+def path_search(game_map: Map, me: Player, opponent: Player, items: list, new_items: list,
+                heatmap, remaining_turns, depth=50, vehicle=None):
+
     available = {tuple(me.location)}
     rows, cols = game_map.rows, game_map.cols
     paths = [[[] for _ in range(game_map.cols)] for _ in range(game_map.rows)]
     paths[me.location[0]][me.location[1]] = [tuple(me.location)]
     value = [[-1]*game_map.cols for _ in range(game_map.rows)]
     value[me.location[0]][me.location[1]] = 0
-    depth = 50
+    depth = min(50, remaining_turns)
     value_per_item = 10
 
     max_move, max_value = None, -1
@@ -37,7 +71,7 @@ def play_turn(game_map: Map, me: Player, opponent: Player, items: list, new_item
         new_available = set()
         for head in available:
             pre_r, pre_c = head
-            moves = generate_moves(game_map, head)
+            moves = generate_moves(game_map, head, vehicle)
             for move in moves:
                 r, c = move
                 # calculate new value
@@ -54,30 +88,4 @@ def play_turn(game_map: Map, me: Player, opponent: Player, items: list, new_item
                 new_available.add(move)
         available = new_available
     if max_move is None: max_move = me.location
-    print(timer() - start)
-    return f'{max_move[0]},{max_move[1]}'
-
-
-
-
-def play_auction(game_map: Map, me: Player, opponent: Player, items: list, new_items: list, heatmap, remaining_turns):
-    return 0
-
-
-def play_transport(game_map: Map, me: Player, opponent: Player, items: list, new_items: list, heatmap, remaining_turns):
-    return f'{random.randint(0, game_map.rows-1)},{random.randint(0, game_map.cols-1)}'
-
-
-
-def generate_moves(game_map, origin):
-    moves = []
-    for dir in [1, -1]:
-        for axis in [0, 1]:
-            move = [0, 0]
-            move[axis] = dir
-            new_pos = tuple([origin[i] + move[i] for i in range(2)])
-            if new_pos[1] in range(game_map.cols) and \
-                    new_pos[0] in range(game_map.rows) and \
-                    game_map.is_free(*new_pos):
-                moves.append(new_pos)
-    return moves
+    return max_move, max_value
